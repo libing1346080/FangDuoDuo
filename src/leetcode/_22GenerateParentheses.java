b04d373384ca558f55e723c89cffa6831357b30b
libing1346080 <1491604605@qq.com>
2017-06-21 11:48:01 +0800

    leetcoe第22题，产生括号
    首先建立main函数

---
+++ src/leetcode/_22GenerateParentheses.java
@@ -0,0 +1,13 @@
+package leetcode;
+import java.util.*;
+/**
+ * Created by fdd on 2017/6/21.
+ */
+public class _22GenerateParentheses {
+    public static void main(String[] args) {
+
+    }
+    public List<String> generateParenthesis(int n){
+        
+    }
+}
