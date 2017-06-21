d274920f615f5e6d06c413d0cf45f1e94f4b2589
libing1346080 <1491604605@qq.com>
2017-06-21 16:25:10 +0800

    利用递归解决第22题
    利用递归解决了括号这个问题，很简单的一个问题。

--- src/leetcode/_22GenerateParentheses.java
+++ src/leetcode/_22GenerateParentheses.java
@@ -5,9 +5,24 @@ import java.util.*;
  */
 public class _22GenerateParentheses {
     public static void main(String[] args) {
-
+        System.out.println(generateParenthesis(3));
     }
-    public List<String> generateParenthesis(int n){
-        
+    public static List<String> generateParenthesis(int n){
+        List<String> res = new ArrayList<>();
+        if(n <= 0) return res;
+        parenthesisCore(res, "", 0, 0, n);
+        return res;
     }
-}
+    public static void parenthesisCore(List<String> res, String str, int left, int right, int max){
+        if(str.length() == max *  2){
+            res.add(str);
+            return;
+        }
+        if(left < max){
+            parenthesisCore(res, str + "(" , left + 1, right, max);
+        }
+        if(right < left){
+            parenthesisCore(res, str + ")" , left, right + 1, max);
+        }
+    }
+ }
