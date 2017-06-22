7e71d6da4a478702fd6169c5a6806aeb348e28f0
libing1346080 <1491604605@qq.com>
2017-06-22 13:07:57 +0800

    leetcode第26题
    不分配额外的内存，去除重复的元素，采用两个指针

---
+++ src/leetcode/RemoveDuplicateElements26.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * Created by fdd on 2017/6/22.
+ */
+public class RemoveDuplicateElements26 {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+        System.out.println(removeDuplicate(nums));
+    }
+    public static int removeDuplicate(int[] nums){
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return 1;
+        int count = 1;
+        int cur = 0, next  = 1;
+        while(next < nums.length){
+            if(nums[cur] == nums[next]){
+                next++;
+            }else{
+                count++;
+                nums[++cur] = nums[next];
+                next++;
+            }
+        }
+        return count;
+    }
+}
